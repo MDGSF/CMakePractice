e050bcb1c8d1a5a91cbdf1777bd98f68e3016079
HuangJian <1342042894@qq.com>
2022-03-01 11:57:29 +0800

    update cg build script

--- cg/build.sh
+++ cg/build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-mkdir build
-
+if [ ! -d "build" ]; then
+  mkdir build
+fi
 cd build
 
 cmake \
   .. \
   -DCMAKE_BUILD_TYPE=Release
 
-make
+make -j4
 
 # 输出详细日志
 # make VERBOSE=1
