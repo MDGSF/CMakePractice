cc9afa862052cf83f9ca7363ab3cc88cf5191844
HuangJian <1342042894@qq.com>
2022-03-02 19:53:12 +0800

    add irrKlang

--- cg/learnopengl/7.in_practice/3.Breakout/game.cpp
+++ cg/learnopengl/7.in_practice/3.Breakout/game.cpp
@@ -1,1 +1,34 @@
 #include "game.h"
+
+Game::Game(unsigned int width, unsigned int height) {
+}
+
+Game::~Game() {
+}
+
+void Game::Init() {
+}
+
+void Game::ProcessInput(float dt) {
+}
+
+void Game::Update(float dt) {
+}
+
+void Game::Render() {
+}
+
+void Game::DoCollisions() {
+}
+
+void Game::ResetLevel() {
+}
+
+void Game::ResetPlayer() {
+}
+
+void Game::SpawnPowerUps(GameObject &block) {
+}
+
+void Game::UpdatePowerUps(float dt) {
+}
--- cg/learnopengl/7.in_practice/3.Breakout/game.h
+++ cg/learnopengl/7.in_practice/3.Breakout/game.h
@@ -1,4 +1,65 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <vector>
+#include <tuple>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+#include "game_level.h"
+#include "power_up.h"
+
+enum GameState {
+  GAME_ACTIVE,
+  GAME_MENU,
+  GAME_WIN,
+};
+
+enum Direction {
+  UP,
+  RIGHT,
+  DOWN,
+  LEFT,
+};
+
+// <collision?, what direction?, difference vector center - closest point>
+typedef std::tuple<bool, Direction, glm::vec2> Collision;
+
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
+
+const float PLAYER_VELOCITY(500.0f);
+
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+
+const float BALL_RADIUS = 12.5f;
+
+class Game {
+public:
+  GameState State;
+  bool Keys[1024];
+  bool KeysProcessed[1024];
+  unsigned int Width;
+  unsigned int Height;
+  std::vector<GameLevel> Levels;
+  std::vector<PowerUp> PowerUps;
+  unsigned int Level;
+  unsigned int Lives;
+
+  Game(unsigned int width, unsigned int height);
+  ~Game();
+  void Init();
+  // game loop
+  void ProcessInput(float dt);
+  void Update(float dt);
+  void Render();
+  void DoCollisions();
+  // reset
+  void ResetLevel();
+  void ResetPlayer();
+  // powerups
+  void SpawnPowerUps(GameObject &block);
+  void UpdatePowerUps(float dt);
+};
+
 #endif
